efa2d937f13eb79384869597cc7f41e3915f6eb7
trunc8or <roryobrien2905@gmail.com>
2026-06-22 06:30:30 +1000

    Update setup.py
    Full first attempt

--- setup.py
+++ setup.py
@@ -1,1 +1,48 @@
+# Installs python packages, launch files, config files, package.xml, 
 
+from setuptools import setup
+from glob import glob
+
+package_name = 'localization'
+
+setup(
+name=package_name,
+version='0.1.0',
+packages=[package_name],
+
+```
+data_files=[
+    ('share/ament_index/resource_index/packages',
+     ['resource/' + package_name]),
+
+    ('share/' + package_name,
+     ['package.xml']),
+
+    ('share/' + package_name + '/launch',
+     glob('launch/*.py')),
+
+    ('share/' + package_name + '/config',
+     glob('config/*')),
+],
+
+install_requires=['setuptools'],
+
+zip_safe=True,
+
+maintainer='Your Name',
+maintainer_email='your_email@example.com',
+
+description='Localization package for MIRTE',
+
+license='MIT',
+
+tests_require=['pytest'],
+
+entry_points={
+    'console_scripts': [
+        'localization_node = localization.localization_node:main',
+    ],
+},
+```
+
+)
