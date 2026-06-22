223013b1aca661a5324702c3b5e88a2880f51777
trunc8or <roryobrien2905@gmail.com>
2026-06-22 10:53:06 +1000

    Update localization_node.py
    Changing it all

--- localization/localization_node.py
+++ localization/localization_node.py
@@ -1,6 +1,24 @@
-def main():
-    print("Localization node started")
+import rclpy
+from rclpy.node import Node
 
 
-if __name__ == "__main__":
+class LocalizationNode(Node):
+
+    def __init__(self):
+        super().__init__('localization_node')
+        self.get_logger().info('Localization node started')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = LocalizationNode()
+
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
     main()
